dc06a0328bd64f8a587ce1a38571b8c44e9f4bd9
Ronald D. Kurr <kurr@kurron.org>
2012-10-26 10:12:25 -0400

    Initial build infrastructure set up.

---
+++ src/test/groovy/org/kurron/jasper/JasperReportsLearningTest.groovy
@@ -0,0 +1,19 @@
+package org.kurron.jasper
+
+
+import spock.lang.Specification;
+
+/**
+ * Trying to figure out how the iText library works.
+ */
+class JasperReportsLearningTest extends Specification
+{
+    def "hello"()
+    {
+        given: "document"
+
+        when: "page is built"
+
+        then: "pdf file is created"
+    }
+}
